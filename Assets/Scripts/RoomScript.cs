d05a1f33c7e9b5c2edb113de5494522bcc1229b0
Tspangler1234 <tspangler1234@gmail.com>
2024-11-06 20:53:37 -0500

    Started Room script

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -8,11 +8,13 @@ public class RoomScript : MonoBehaviour
     public int numEnemies;
     public GameObject[] EnemyPrefabs;
     public Vector2[] EnemyPositions;
+    [SerializeField] private GameObject Blocker;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Blocker.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -23,11 +25,11 @@ public class RoomScript : MonoBehaviour
 
     public void PlayerEnter()
     {
-
+        Blocker.SetActive(true);
     }
 
     public void RoomCleared()
     {
-
+        Blocker.SetActive(false);
     }
 }
