8d1e34a127fb8c07478f2bd54ec334b46bc157a1
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2024-11-09 15:30:44 -0500

    shop opens/closes on player trigger enter/exit

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -11,16 +11,39 @@ public class ShopScript : MonoBehaviour
     public TextMeshProUGUI Pricetxt;
     public TextMeshProUGUI Quantitytxt;
     public GameObject ShopManager;
+    [SerializeField] private Canvas shopCanvas;
+
+    private void Start()
+    {
+        shopCanvas = GetComponentInChildren<Canvas>();
+        shopCanvas.enabled = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Pricetxt.text = "Price: $" + ShopManager.GetComponent<ShopManager>().items[2, ItemID].ToString();
-        Quantitytxt.text = ShopManager.GetComponent<ShopManager>().items[2, ItemID].ToString();
+        //Pricetxt.text = "Price: $" + ShopManager.GetComponent<ShopManager>().items[2, ItemID].ToString();
+        //Quantitytxt.text = ShopManager.GetComponent<ShopManager>().items[2, ItemID].ToString();
     }
 
     public void DisplayRandomItems()
     {
 
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            shopCanvas.enabled = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            shopCanvas.enabled = false;
+        }
     }
 }
