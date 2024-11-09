136e46734b79bd9bda6c7017b7fafcbc4fab1ed1
Tspangler1234 <tspangler1234@gmail.com>
2024-11-08 20:40:55 -0500

    Started Shop mechanic

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -1,22 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopScript : MonoBehaviour
 {
-    //variables
-    public GameObject[] items;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int ItemID;
+    public TextMeshProUGUI Pricetxt;
+    public TextMeshProUGUI Quantitytxt;
+    public GameObject ShopManager;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Pricetxt.text = "Price: $" + ShopManager.GetComponent<ShopManager>().items[2, ItemID].ToString();
+        Quantitytxt.text = ShopManager.GetComponent<ShopManager>().items[2, ItemID].ToString();
     }
 
     public void DisplayRandomItems()
