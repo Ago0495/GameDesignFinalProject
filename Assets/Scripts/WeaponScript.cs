dcf130898d29b6aae69d882ef5e65ca1afe083d7
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2024-11-09 16:33:43 -0500

    new attack and cooldown animation

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -7,15 +7,18 @@ public class WeaponScript : MonoBehaviour
     //variables
     [SerializeField] private int atkDamage;
     [SerializeField] private float atkRange;
-    [SerializeField] private float atkCooldown;
     [SerializeField] private float atkKnockbackForce;
+    [SerializeField] private float atkCooldown;
     [SerializeField] private float handling;
     //[SerializeField] private int weaponTag;
     //[SerializeField] private int upgrade;
     private Collider2D weaponCollider;
     private Animator animator;
     private bool onCooldown;
+    private float attackAnimationTime;
+    private float cooldownAnimationTime;
     private List<GameObject> alreadyHitList;
+    private Color col;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,9 @@ public class WeaponScript : MonoBehaviour
         alreadyHitList = new List<GameObject>();
         weaponCollider.enabled = false;
         onCooldown = false;
+        col = GetComponent<Renderer>().material.color;
+
+        UpdateAnimClipTimes();
     }
 
     // Update is called once per frame
@@ -42,12 +48,13 @@ public class WeaponScript : MonoBehaviour
             alreadyHitList.Clear();
 
             //start coroutine to turn off collider
-            StartCoroutine(WeaponCooldown(10f / handling));
+            StartCoroutine(WeaponCooldown((10f / handling) * attackAnimationTime));
             onCooldown = true;
 
             //play swing animation
             animator.SetFloat("atkSpeed", (handling / 10f));
-            animator.SetBool("attack", true);
+            //animator.SetBool("attack", true);
+            animator.Play("AttackAnimation");
         }
     }
 
@@ -74,13 +81,30 @@ public class WeaponScript : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         weaponCollider.enabled = false;
-        animator.SetBool("attack", false);
 
-        yield return new WaitForSeconds(atkCooldown);
+        animator.SetFloat("atkCooldown", 1f / atkCooldown);
+        yield return new WaitForSeconds((atkCooldown / 1f) * cooldownAnimationTime);
         onCooldown = false;
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
+    public void UpdateAnimClipTimes()
+    {
+        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+        foreach (AnimationClip clip in clips)
+        {
+            switch (clip.name)
+            {
+                case "AttackAnimation":
+                    attackAnimationTime = clip.length;
+                    break;
+                case "CooldownAnimation":
+                    cooldownAnimationTime = clip.length;
+                    break;
+            }
+        }
+    }
+
+        public void OnTriggerEnter2D(Collider2D other)
     {
         EntityScript otherEntity = other.gameObject.GetComponent<EntityScript>();
 
