58640d45391acb334f296383ad3abec7ed079fef
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2024-11-09 21:40:50 -0500

    added new font

--- Assets/Scripts/ShopItemScript.cs
+++ Assets/Scripts/ShopItemScript.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopItemScript : MonoBehaviour
 {
+    private int itemPrice;
+    private string itemName;
+    private Image itemImage;
+    private TextMeshProUGUI itemNameText;
+    private TextMeshProUGUI itemPriceText;
+
     // Start is called before the first frame update
     void Start()
     {
