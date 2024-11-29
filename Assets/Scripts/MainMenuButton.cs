a7610017cdae5c4c4bf590d6421dda069b1f2b2e
EthanDiemer <119145667+EthanDiemer@users.noreply.github.com>
2024-11-29 14:23:45 -0500

    MainMenuButtun deletes the player and GameManager.

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -6,6 +6,12 @@ public class MainMenuButton : MonoBehaviour
 {
     public void OnClick()
     {
+        GameObject player = GameObject.Find("/Player");
+        Destroy(player);
+
+        GameObject gamemanager = GameObject.Find("/GameManager");
+        Destroy(gamemanager);
+
         Level mainMenu = GameManager.FindInLevels("MainTitleScene");
         int mainMenuNumber = mainMenu.GetLevelNumber();
         GameManager.SwitchLevelTo(mainMenuNumber);
