2f755441624a794ca3d3052f845ce00c9eb1d14d
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2024-12-05 16:56:57 -0500

    Changed Dialogue
    Dialogue properly displays names

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
-    private string name;
-
+    [SerializeField] private string name;
     [SerializeField] public Transform speaker;
     [TextArea(3,10)]
     public string[] sentences;
--- Assets/Scripts/Dialogue/DialogueOptions.cs
+++ Assets/Scripts/Dialogue/DialogueOptions.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class DialogueOptions : MonoBehaviour
 {
-    public string entityName;
     public Dialogue[] dialogueList;
 
-    void Start()
-    {
-        foreach (var dialogue in dialogueList)
-        {
-            dialogue.SetName(entityName);
-        }
-    }
     public void PickDialogue(int dialogueNumber)
     {
         if (dialogueList.Length > 0 && dialogueNumber < dialogueList.Length)
